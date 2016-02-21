cfc6635671f938aea5982575dbd5fc928b50a890
Olybri <loris.wit@gmail.com>
2016-02-21 14:56:38 +0100

    Ajout d'une liste des joueurs connectés

--- index.php
+++ index.php
@@ -32,14 +32,12 @@
                 <?php
                     error_reporting(E_ERROR | E_PARSE);
 
-                    $address = gethostbyname(HOSTNAME);
-
                     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-                    socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array("sec" => 5, "usec" => 0)); 
-                    socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec" => 5, "usec" => 0)); 
+                    socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array("sec" => 5, "usec" => 0));
+                    socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec" => 5, "usec" => 0));
 
                     echo "<h2>Le serveur est ";
-                    if(socket_connect($socket, $address, PORT) === false)
+                    if(socket_connect($socket, HOSTNAME, PORT) === false)
                     {
                         echo "<span class=\"red\">fermé</span>.</h2><br>";
                     }
@@ -47,11 +45,35 @@
                     {
                         echo "<span class=\"green\">ouvert</span>.</h2><br>";
 
-                        socket_send($socket, "\xFE", 1, 0);
-                        socket_recv($socket, $data, 512, 0);
-                        $pieces = explode("\x00\xA7", substr($data, 1));
-                        echo "Message : <b>$pieces[0]</b><br>"
-                            ."Joueurs connectés : <b>$pieces[1]</b> / <b>$pieces[2]</b><br>";
+                        $handshake = pack("ccca*nccc", 0, 47, strlen(HOSTNAME), HOSTNAME, PORT, 1, 1, 0);
+                        $handshake = chr(strlen($handshake) - 2).$handshake;
+
+                        socket_write($socket, $handshake);
+
+                        while(ord(socket_read($socket, 1)) != 0);
+
+                        for($i = 0, $size = 0; ($byte = ord(socket_read($socket, 1))) >= 128; $i++)
+                            $size = ($byte - 128) * pow(256, $i);
+
+                        $size += $byte * pow(256, $i);
+
+                        socket_recv($socket, $response, $size, 0);
+                        if(substr(response, -2) != "\"}")
+                            $response .= "\"}";
+
+                        $info = json_decode($response);
+
+                        echo "Message : <i>".$info->{"description"}."</i><br>";
+
+                        if($info->{"players"}->{"online"} > 0)
+                        {
+                            foreach($info->{"players"}->{"sample"} as $name)
+                                $playerlist .= "<b>".$name->{"name"}."</b> <img src=\"https://crafatar.com/avatars/".$name->{"name"}."?size=16&default=MHF_Steve&overlay\"> | ";
+
+                            echo "<b>".$info->{"players"}->{"online"}."</b> joueurs connectés :<br>".substr($playerlist, 0, -3)."<br>";
+                        }
+                        else
+                            echo "Aucun joueur connecté.<br>";
                     }
                     socket_close($socket);
 
